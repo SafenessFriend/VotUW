af6e0ffcd445eb63a2848ef662f5514bf4799bd6
Jack <booker.jack@gmail.com>
2018-04-30 21:29:44 +1000

    player character
    Velocity/gravity based character control with raycast collision and beginnings of an animation controller

---
+++ Valley/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Controller2D)) ]
+public class Player : MonoBehaviour {
+
+    public float jumpHeight = 4;
+    public float timeToJumpApex = 0.4f;
+    public float moveSpeed = 6f;
+        
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+
+    Controller2D controller;
+
+    Animator anim;
+
+    void Start ()
+    {
+        controller = GetComponent<Controller2D>();
+        anim = GetComponent<Animator>();
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        anim.SetFloat("playerSpeed", velocity.x);
+	}
+}
