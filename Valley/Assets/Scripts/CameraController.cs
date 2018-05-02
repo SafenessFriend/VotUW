2e99e8c1e0ea6a4242b16e14896ff529ef7653db
PowerTruck <38897643+PowerTruck@users.noreply.github.com>
2018-05-02 18:17:56 +1000

    Camera Controller
    Re-adding camera controller.

---
+++ Valley/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    // game object for camera to follow
+    public GameObject targetObj;
+
+    // offset value to track object
+    Vector3 offset;
+
+    //camera position relative to target
+    public Vector3 cameraPosition;
+
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - targetObj.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = targetObj.transform.position + offset + cameraPosition;
+	}
+}
